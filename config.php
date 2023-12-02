23138824f66ecf505fef65ffa4bf101c22cf8c57
mtahakhan07 <mtaha.khan2004@gmail.com>
2023-12-02 23:44:09 +0500

    Revert "Just started working"
    This reverts commit 62d714f8cb87f7675a8da445238e689041b9c225.

--- config.php
+++
@@ -1,6 +0,0 @@
-<?php
-// Configuration settings
-$userAgent = 'YourCrawlerBot/1.0';
-$maxDepth = 5; // Adjust as needed
-
-?>
--- index.php
+++
@@ -1,9 +0,0 @@
-<?php
-include 'config.php';
-include 'crawler.php';
-
-// Main entry point logic here
-$startUrl = 'https://example.com';
-crawl($startUrl);
-
-?>
