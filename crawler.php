62d714f8cb87f7675a8da445238e689041b9c225
mtahakhan07 <mtaha.khan2004@gmail.com>
2023-12-02 23:26:31 +0500

    Just started working

---
+++ crawler.php
@@ -0,0 +1,29 @@
+<?php
+include 'utils.php';
+include 'queue.php';
+
+function crawl($url, $depth = 0) {
+    global $maxDepth;
+
+    if ($depth > $maxDepth) {
+        return;
+    }
+
+    // Your crawling logic here
+    $html = fetchHtml($url);
+    $links = extractLinks($html);
+
+    foreach ($links as $link) {
+        enqueue($link, $depth + 1);
+    }
+
+    // Extract and process data from $html
+
+    // Continue crawling for each link in the queue
+    while ($nextUrl = dequeue()) {
+        crawl($nextUrl['url'], $nextUrl['depth']);
+    }
+}
+
+// Other functions for making HTTP requests, parsing HTML, etc.
+?>
---
+++ index.php
@@ -0,0 +1,9 @@
+<?php
+include 'config.php';
+include 'crawler.php';
+
+// Main entry point logic here
+$startUrl = 'https://example.com';
+crawl($startUrl);
+
+?>
